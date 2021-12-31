8f2e9d76ac052703a4adcafe6f3f8582ddbce21b
LimaBD <bdl1998@hotmail.com>
2021-12-31 19:44:02 -0300

    platforms first commit

---
+++ platforms.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+#
+# This file contains code to download files
+# from platforms and other functionalities
+
+
+"""
+This file contains code to download files
+from platforms and other functionalities
+"""
+
+
+class platform:
+	"""Template class for platform"""
+
+	base_url = ''
+
+
+	def getAllFiles(folder):
+		"""This is used to get all files from a web folder"""
+
+
+	def download(target_file, new_filename):
+		"""This is used to download a file"""
+
+
+class GoogleDrive(platform):
+	"""Google Drive platform class"""
