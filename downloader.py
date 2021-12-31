746b7230e37af4d70894f3a7f8dd7cb3b9804c4d
LimaBD <bdl1998@hotmail.com>
2021-12-31 19:43:36 -0300

    added main class

--- downloader.py
+++ downloader.py
@@ -1,6 +1,4 @@
-# #! /usr/bin/env python
-#
-# Downloader
+#! /usr/bin/env python
 #
 # This is a console tool usefull to 
 # download large amounts of files
@@ -10,3 +8,17 @@
 This is a console tool usefull to
 download large amounts of files
 """
+
+
+class downloader:
+	"""
+	This is a console tool usefull to
+	download large amounts of files
+	"""
+
+	# TODO: add args processing
+	# TODO: download functionality
+
+
+if __name__ == '__main__':
+	# TODO: add call to start running downloader.
