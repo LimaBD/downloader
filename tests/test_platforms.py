0a296a5a3814f02d1944bffc4b65b5b25ecb32a9
LimaBD <bdl1998@hotmail.com>
2021-12-31 19:44:16 -0300

    added tests

---
+++ tests/test_platforms.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+#
+# This is used to tests platforms module.
+
+
+"""Tests for platforms module"""
+
+
+# Standard packages
+import unittest
+
+
+# Local packages
+import platforms
+
+
+class TestPlatforms():
+	""" """
+
+	# TODO: add unit tests
+
+
+if __name__ == '__main__':
+    unittest.main()
