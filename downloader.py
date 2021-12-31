82027fde25a7841df753e4ca32f9996027fafa32
LimaBD <bdl1998@hotmail.com>
2021-12-31 19:01:05 -0300

    changed filename to downloader.py

---
+++ downloader.py
@@ -0,0 +1,12 @@
+# #! /usr/bin/env python
+#
+# Downloader
+#
+# This is a console tool usefull to 
+# download large amounts of files
+
+
+"""
+This is a console tool usefull to
+download large amounts of files
+"""
